621a6c8171fd50a072a789e8e0a07a012c92c61a
Anjali <68515341+peachycloudsecurity@users.noreply.github.com>
2026-01-09 17:50:50 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,8 +17,8 @@ const Index = () => {
       <Hero />
       <About />
       <Schedule />
-      <CFP />
       <Volunteers />
+      <CFP />
       <Sponsors />
       <FAQ />
       <CodeOfConduct />
